815ee508860d1a1b1797524aaddc8ce2fc586e6a
fozie <fozie@Francky>
2023-12-19 20:56:41 +0100

    first project commit

---
+++ src/BB/Main.java
@@ -0,0 +1,20 @@
+package BB;
+
+import javax.swing.JFrame;
+
+// Here the game window is created, i used JFrame to create the 
+//windows bounds and its size
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame obj = new JFrame(); // here we create a new object from the cklass JFrame
+		GamePlay gamePlay = new GamePlay(); // creating the Gameplay object
+		obj.setBounds(10,10,700,600); // the boundaries of the Game
+		obj.setTitle("Franck Brick breacker");
+		obj.setResizable(false); // to make sure the windows is resizable
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
